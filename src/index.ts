9951bfe5716a4e6c5e6d8c6804d182196377dfa0
ComradeVanti <kingvanti650@gmail.com>
2022-06-15 14:30:27 +0200

    feat: dotenv support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import 'dotenv/config'
+
 const app = express();
-const PORT = 3000;
+const PORT = parseInt(process.env.PORT || "3000");
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
+
