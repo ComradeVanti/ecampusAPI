9092e90d4ada73d5865699ec3b4b719855d503e4
ComradeVanti <kingvanti650@gmail.com>
2022-06-21 09:06:49 +0200

    feat: add web utility functions

--- src/common/web.ts
+++ src/common/web.ts
@@ -2,6 +2,8 @@ import { Operation } from './operation';
 import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
 import setCookie from 'set-cookie-parser';
 import { Maybe } from './maybe';
+import { Html } from './domain';
+import { JSDOM } from 'jsdom';
 
 export const enum WebErrorType {
   GENERIC = 'GENERIC',
@@ -17,6 +19,17 @@ export const get = <TData, TConfig = any>(
     axios.get(url, config),
     (reason) => ({ type: WebErrorType.GENERIC, reason })
   );
+
+export const getHtml = <TConfig = any>(
+  url: string,
+  config?: AxiosRequestConfig<TConfig>
+) => get<Html, TConfig>(url, config).map<Html>((response) => response.data);
+
+export const getDocument = <TConfig = any>(
+  url: string,
+  config?: AxiosRequestConfig<TConfig>
+) =>
+  getHtml(url, config).map<Document>((html) => new JSDOM(html).window.document);
 
 export const tryGetCookieFrom = (response: AxiosResponse, cookie: string) => {
   const rawCookies = response.headers[cookie] ?? [];
